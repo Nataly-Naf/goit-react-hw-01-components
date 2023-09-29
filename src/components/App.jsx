fda0853d6b6098c29a62a6552c6e49ee3fccfad6
Nataly-Naf <nat.spivak84@gmail.com>
2023-09-29 12:30:36 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
 import { Profile } from "./Profile/Profile";
 import * as item from '../user.json'
-import data from '../data.json'
+import * as data from '../data.json'
 import { Statistics } from "./Statistics/Statistics";
 import { FriendList } from './FriendList/FriendList';
-import  friends  from '../friends.json'
-import items from '../transactions.json'
+import  * as friends from '../friends.json'
+import * as items from '../transactions.json'
 import {TransactionHistory} from '..//components/TransactionHistory/TransactionHistory'
 export const App = () => {
   return (
